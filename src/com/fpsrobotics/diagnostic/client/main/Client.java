6362a60755d518d00e9b33a0a68d4ca4db55c530
Jon <>
2018-12-27 22:48:57 -0500

    tried to solve timeouts after 30 seconds, failed

--- src/com/fpsrobotics/diagnostic/client/main/Client.java
+++ src/com/fpsrobotics/diagnostic/client/main/Client.java
@@ -16,6 +16,7 @@ public class Client {
 	static String message;
 	static Scanner scanner = new Scanner(System.in);
 	static int port = 5800;
+	private static boolean isTimeoutOn = true;
 	// String username = scanner.nextLine();
 
 	public static void main(String args[]) {//TODO Delete this line
@@ -31,14 +32,19 @@ public class Client {
         	execute();
     	}
     	else if(input.equalsIgnoreCase("connect local")) {
-    		System.out.println("Connecting to a local server on port "+port);
+    		System.out.println("Connecting to local server on port "+port);
     		executeTest();
     	}
     	else if(input.equalsIgnoreCase("exit")) {
     		System.exit(0);
     }
+    	else if(input.equalsIgnoreCase("notimeout")) {
+    		System.out.println("There's no way to turn this off");
+    		isTimeoutOn = false;
+    		parseInput();
+    		}
     	else {
-    	System.out.println("use 'exit', 'connect', 'connect local'");
+    	System.out.println("use 'exit', 'connect', 'connect local', 'notimeout'");
     	parseInput();
     	}
     }
@@ -65,6 +71,7 @@ public class Client {
 			String message = br.readLine();
 			System.out.println("ROBOT:  " + message);
 			execute();//start over
+			
 		} catch (Exception exception) {
 			System.out.println(exception.getMessage());
 			//exception.printStackTrace();
@@ -77,6 +84,7 @@ public class Client {
 				e.printStackTrace();
 			}
 		}
+		
 		}
 	public static void executeTest() {
 		try {
@@ -100,10 +108,18 @@ public class Client {
 			InputStreamReader isr = new InputStreamReader(is);
 			BufferedReader br = new BufferedReader(isr);
 			String message = br.readLine();
-			System.out.println("ROBOT:  " + message);
+			System.out.println("ROBOT: " + message);
 			execute();//start over
+			
 		} catch (Exception exception) {
-			System.out.println(exception.getMessage());
+			if(exception.getMessage().equals("Connection timed out: connect\r\n" + "")) { //TODO Fix this so there are no more timeouts
+				System.out.println("The connection timed out, reconnecting...");
+				executeTest();
+			}
+			else {
+				System.out.println(exception.getMessage());
+
+			}
 			//exception.printStackTrace();
 			parseInput();
 		} finally {
